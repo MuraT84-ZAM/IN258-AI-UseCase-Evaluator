e27ac3036662fe47c713e522a326d359f64a2edb
MuraT84-ZAM <murat.yildizdogan@hftm.ch>
2026-09-08 10:49:38 +0200

    MongoDB-Verbindung aus Java hergestellt und getestet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo, das Projekt laeuft.");
+        String verbindung = "mongodb://localhost:27017";
+
+        try (MongoClient client = MongoClients.create(verbindung)) {
+            MongoDatabase datenbank = client.getDatabase("ai_usecase_evaluator");
+
+            System.out.println("Verbindung steht.");
+            System.out.println("Datenbank: " + datenbank.getName());
+            System.out.println("Anzahl Use Cases: " + datenbank.getCollection("usecases").countDocuments());
+        }
     }
 }
